05ddb712cffbe22888bf4cc50ff8944c21e877a6
Visamber <vismaberdeveloper@gmail.com>
2026-01-25 18:51:59 +0530

    Created initial 'CreateNewPasswordScreen' composable

---
+++ app/src/main/java/com/lazysloth/pocketlog/ui/screen/CreateNewPasswordScreen.kt
@@ -0,0 +1,91 @@
+package com.lazysloth.pocketlog.ui.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.lazysloth.pocketlog.R
+import com.lazysloth.pocketlog.ui.theme.PocketLogTheme
+
+@Composable
+fun CreateNewPasswordScreen() {
+    var createNewPassword by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(
+            value = createNewPassword,
+            onValueChange = { createNewPassword = it },
+            label = {
+                Text(
+                    stringResource(R.string.create_new_password)
+                )
+            },
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            ),
+            shape = RoundedCornerShape(10.dp),
+            singleLine = true,
+            modifier = Modifier.width(280.dp)
+        )
+        Spacer(Modifier.height(10.dp))
+        TextField(
+            value = confirmPassword,
+            onValueChange = { confirmPassword = it },
+            label = {
+                Text(
+                    stringResource(R.string.confirm_password)
+                )
+            },
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            ),
+            shape = RoundedCornerShape(10.dp),
+            singleLine = true,
+            modifier = Modifier.width(280.dp)
+        )
+        Spacer(Modifier.height(10.dp))
+        Button(onClick = {}) {
+            Text(
+                stringResource(R.string.done)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CreateNewPasswordScreenPreview() {
+    PocketLogTheme {
+        CreateNewPasswordScreen()
+    }
+}
